eeabfb267019014ea67094fcbb3743882d564092
AlejoGalizzi <alejogalizzi@gmail.com>
2024-04-18 17:47:45 -0300

    Create team entity and error response.

---
+++ src/main/java/com/alejogalizzi/teams/model/response/ErrorResponse.java
@@ -0,0 +1,18 @@
+package com.alejogalizzi.teams.model.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponse {
+
+  private String mensaje;
+
+  private Integer codigo;
+
+}
