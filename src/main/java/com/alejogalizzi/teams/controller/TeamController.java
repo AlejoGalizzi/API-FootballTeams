b41d34277c5d6c63135ead07250544df8bd313ac
AlejoGalizzi <alejogalizzi@gmail.com>
2024-04-22 10:56:52 -0300

    Add documentation to endpoints.

--- src/main/java/com/alejogalizzi/teams/controller/TeamController.java
+++ src/main/java/com/alejogalizzi/teams/controller/TeamController.java
@@ -1,7 +1,10 @@
 package com.alejogalizzi.teams.controller;
 
 import com.alejogalizzi.teams.model.entity.Team;
+import com.alejogalizzi.teams.model.response.ErrorResponse;
 import com.alejogalizzi.teams.service.implementations.TeamService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
@@ -27,32 +30,66 @@ public class TeamController {
   @Autowired
   private TeamService equipoService;
 
+  @Operation(summary = "Consulta de Todos los Equipos")
   @GetMapping(produces = "application/json")
   public ResponseEntity<List<Team>> getTeams() {
     return ResponseEntity.ok(equipoService.findAllTeams());
   }
 
+
+  @Operation(summary = "Consulta de un Equipo por ID")
   @GetMapping(value = "/{id}",produces = "application/json")
+  @ApiResponses(value = {
+    @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "Equipo encontrado"),
+    @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "404", description = "Equipo no encontrado",
+        content = @io.swagger.v3.oas.annotations.media.Content(schema = @io.swagger.v3.oas.annotations.media.Schema(implementation = ErrorResponse.class)))
+  })
   public ResponseEntity<Team> getTeamById(@PathVariable Long id) {
     return new ResponseEntity<>(equipoService.findTeamById(id), HttpStatus.OK);
   }
 
+  @Operation(summary = "Búsqueda de Equipos por Nombre")
   @GetMapping(value = "/buscar", produces = "application/json")
+  @ApiResponses(value = {
+      @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "Equipo encontrado"),
+      @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "404", description = "Equipo no encontrado",
+          content = @io.swagger.v3.oas.annotations.media.Content(schema = @io.swagger.v3.oas.annotations.media.Schema(implementation = ErrorResponse.class)))
+  })
   public ResponseEntity<List<Team>> getTeamByName(@RequestParam("nombre") String name) {
     return new ResponseEntity<>(equipoService.findTeamByName(name), HttpStatus.OK);
   }
 
+  @Operation(summary = "Creación de un Equipo")
   @PostMapping(produces = "application/json")
+  @ApiResponses(value = {
+      @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "Equipo encontrado"),
+      @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", description = "La solicitud es invalida.",
+          content = @io.swagger.v3.oas.annotations.media.Content(schema = @io.swagger.v3.oas.annotations.media.Schema(implementation = ErrorResponse.class)))
+  })
   public ResponseEntity<Team> saveTeam(@RequestBody Team team) {
     return new ResponseEntity<>(equipoService.saveTeam(team), HttpStatus.CREATED);
   }
 
+  @Operation(summary = "Actualización de Información de un Equipo")
+  @ApiResponses(value = {
+      @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "Equipo encontrado"),
+      @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "404", description = "Equipo no encontrado",
+          content = @io.swagger.v3.oas.annotations.media.Content(schema = @io.swagger.v3.oas.annotations.media.Schema(implementation = ErrorResponse.class))),
+      @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", description = "La solicitud es invalida.",
+          content = @io.swagger.v3.oas.annotations.media.Content(schema = @io.swagger.v3.oas.annotations.media.Schema(implementation = ErrorResponse.class)))
+  })
   @PutMapping(value = "/{id}", produces = "application/json")
   public ResponseEntity<Team> updateTeam(@PathVariable Long id, @RequestBody Team team) {
     return new ResponseEntity<>(equipoService.updateTeam(id, team), HttpStatus.OK);
   }
 
+  @Operation(summary = "Eliminación de un Equipo")
   @DeleteMapping(value = "/{id}", produces = "application/json")
+  @ApiResponses(value = {
+      @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "Equipo encontrado"),
+      @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "404", description = "Equipo no encontrado",
+          content = @io.swagger.v3.oas.annotations.media.Content(schema = @io.swagger.v3.oas.annotations.media.Schema(implementation = ErrorResponse.class)))
+  })
   public ResponseEntity<Void> deleteTeam(@PathVariable Long id) {
     equipoService.deleteTeam(id);
     return new ResponseEntity<>(HttpStatus.NO_CONTENT);
